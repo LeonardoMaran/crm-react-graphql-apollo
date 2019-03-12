8d87f6f8fc5016ff36264720a199bc8eab10c200
Agustin Navarro <xAgustin93@gmail.com>
2019-03-13 00:08:32 +0100

    Servidor Apollo

---
+++ servidor/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+// GraphQL
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs } from './data/schema';
+import { resolvers } from './data/resolvers';
+
+const app = express();
+
+const server = new ApolloServer({typeDefs, resolvers});
+
+server.applyMiddleware({app});
+
+app.listen({port: 4000}, () => console.log(`El servidor esta funcionando: http://localhost:4000${server.graphqlPath}`))
