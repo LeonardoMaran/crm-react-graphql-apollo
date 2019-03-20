249eda015c7a07f013ef6d0136f67135132fe56b
AgustinNG <AgustinNavarro@cecotec.es>
2019-03-20 17:09:29 +0100

    Leyendo los Datos desde el Formulario

--- cliente/src/components/Productos/NuevoProducto/NuevoProducto.js
+++ cliente/src/components/Productos/NuevoProducto/NuevoProducto.js
@@ -1,11 +1,78 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 
-class Productos extends Component {
+class NuevoProducto extends Component {
+
+    state = {
+        nombre: '',
+        precio: '',
+        stock: ''
+    }
+
+    actualizarState = (e) => {
+        const { name, value } = e.target;
+
+        this.setState({
+            [name] : value
+        })
+        
+        // const { precio, value } = e.target;
+        // const { stock, value } = e.target;
+    }
+
     render() {
         return (
-            <div>Productos Component</div>
+            <Fragment>
+
+                <h1 className="text-center mb-5">Nuevo Producto</h1>
+
+                <div className="row justify-content-center">
+                    <form className="col-md-8">
+                        <div className="form-group">
+                            <label>Nombre:</label>
+                            <input 
+                                type="text"
+                                name="nombre" 
+                                className="form-control" 
+                                placeholder="Nombre del Producto"
+                                onChange={this.actualizarState}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label>Precio:</label>
+                            <div className="input-group">
+                                <div className="input-group-prepend">
+                                    <div className="input-group-text">$</div>
+                                </div>
+                                <input 
+                                    type="number" 
+                                    name="precio" 
+                                    className="form-control" 
+                                    placeholder="Precio del Producto"
+                                    onChange={this.actualizarState}
+                                />
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <label>Stock:</label>
+                            <input 
+                                type="number" 
+                                name="stock" 
+                                className="form-control" 
+                                placeholder="stock del Producto" 
+                                onChange={this.actualizarState}
+                            />
+                        </div>
+                        <button 
+                            type="submit" 
+                            className="btn btn-success float-right">
+                                Crear Producto
+                        </button>
+                    </form>
+                </div>
+
+            </Fragment>
         )
     }
 }
 
-export default Productos;
+export default NuevoProducto;
