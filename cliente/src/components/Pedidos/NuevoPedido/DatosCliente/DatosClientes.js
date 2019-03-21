5661d9f0f3785d0dbc29035b1655ae668431f0a6
AgustinNG <AgustinNavarro@cecotec.es>
2019-03-21 14:22:37 +0100

    Agregando los Componentes para los Pedidos

---
+++ cliente/src/components/Pedidos/NuevoPedido/DatosCliente/DatosClientes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DatosCliente = () => {
+    return (
+        <h2>Resumen de Cliente</h2>
+    )
+}
+
+export default DatosCliente;
--- cliente/src/components/Pedidos/NuevoPedido/NuevoPedido.js
+++ cliente/src/components/Pedidos/NuevoPedido/NuevoPedido.js
@@ -1,9 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import DatosCliente from './DatosCliente/DatosClientes';
+
+// Components
+
 
 class NuevoPedido extends Component {
     render() {
+        
+        const { id } = this.props.match.params;
+
         return (
-            <h1>Hola</h1>
+            <Fragment>
+                <h1 className="text-center mb-5">Nuevo Pedido</h1>
+
+                <div className="row">
+                    <div className="col-md-3">
+                        <DatosCliente
+                            id={id} />
+                    </div>
+
+                    <div className="col-md-9">
+                        Pedido Aqui
+                    </div>
+                </div>
+            </Fragment>
         )
     }
 }
