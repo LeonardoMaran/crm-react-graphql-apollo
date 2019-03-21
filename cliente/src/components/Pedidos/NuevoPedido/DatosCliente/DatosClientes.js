97e1f7b618d19af3ee2619e70de175f3a797043b
AgustinNG <AgustinNavarro@cecotec.es>
2019-03-21 15:59:33 +0100

    Mostrando los Datos del Cliente que realiza el Pedido

--- cliente/src/components/Pedidos/NuevoPedido/DatosCliente/DatosClientes.js
+++ cliente/src/components/Pedidos/NuevoPedido/DatosCliente/DatosClientes.js
@@ -13,15 +13,44 @@ const DatosCliente = (id) => {
                     if(loading) return 'Cargando...';
                     if(error) return `Error ${error.message}`;
 
-                    console.log(data);
+                    // console.log(data.getCliente);
+
+                    const { nombre, apellido, edad, emails, empresa, tipo } = data.getCliente;
 
                     return (
-                        <h1>Hola</h1>
+                        <ul className="list-unstyled my-5">
+                            <li className="border font-weight-bold p-2">
+                                Nombre: 
+                                <span className="font-weight-normal"> {nombre}</span>
+                            </li>
+                            <li className="border font-weight-bold p-2">
+                                Apellidos: 
+                                <span className="font-weight-normal"> {apellido}</span>
+                            </li>
+                            <li className="border font-weight-bold p-2">
+                                Edad: 
+                                <span className="font-weight-normal"> {edad}</span>
+                            </li>
+                            <li className="border font-weight-bold p-2">
+                                Emails: 
+                                <span className="font-weight-normal">
+                                {emails.map(email => ` ${email.email}` )}
+                                </span>
+                            </li>
+                            <li className="border font-weight-bold p-2">
+                                Empresa: 
+                                <span className="font-weight-normal"> {empresa}</span>
+                            </li>
+                            <li className="border font-weight-bold p-2">
+                                Tipo: 
+                                <span className="font-weight-normal"> {tipo}</span>
+                            </li>
+                        </ul>
                     )
                     
                 }}
             </Query>
-            
+
         </Fragment>
     )
 }
