b155a5ef69e5e7a1ca2db2e5ae707c9e062168f3
AgustinNG <AgustinNavarro@cecotec.es>
2019-03-22 13:38:32 +0100

    Realizando la Consulta para obtener los Pedidos del Cliente

--- cliente/src/components/Pedidos/PedidosCliente/PedidosCliente.js
+++ cliente/src/components/Pedidos/PedidosCliente/PedidosCliente.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
-const PedidosCliente = () => {
+const PedidosCliente = (props) => {
+
+    // console.log(props);
+
+    const clienteId = props.match.params.id;
+    // console.log(clienteId);
+    
+    
+
     return (
-        <p>PedidosCliente Component</p>
+        <Fragment>
+            <h1 className="text-center mb-5">Pedidos del Cliente</h1>
+
+            <div className="row">
+
+            </div>
+        </Fragment>
     )
 }
 export default PedidosCliente;
